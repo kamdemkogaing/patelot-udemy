03f62085aeada1eb4870a76335fe0e1b1c558172
Patrick <patrick-kamdem@patelot.de>
2024-10-07 19:12:43 +0200

    udemy courses

---
+++ 9.React-Redux/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-700 px-4">
+      <div className="mx-auto max-w-3xl pt-10 pb-20">
+        <div className="text-slate-200">
+          <button className="p-4 bg-slate-900 rounded-2xl hover:bg-slate-950 hover:font-bold">
+            Add To Cart
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
